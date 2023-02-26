32566018d9cd53093a8a66b6f725cdf71a6bb59a
sudsarkar13 <67630579+sudsarkar13@users.noreply.github.com>
2023-02-26 16:38:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,22 +55,22 @@ async def error_handler(_, message: Message):
 # Start the client
 client.run()
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+# @app.route('/')
+# def index():
+#     return 'Hello World!'
 
 
-@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
-def respond():
-    update = Update.de_json(request.get_json(force=True), bot)
-    setup().process_update(update)
-    return 'ok'
+# @app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
+# def respond():
+#     update = Update.de_json(request.get_json(force=True), bot)
+#     setup().process_update(update)
+#     return 'ok'
 
-# vercel verify
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+# # vercel verify
+# @app.route('/setwebhook', methods=['GET', 'POST'])
+# def set_webhook():
+#     s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
+#     if s:
+#         return "webhook setup ok"
+#     else:
+#         return "webhook setup failed"
